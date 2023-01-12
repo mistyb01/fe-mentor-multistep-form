210eeaac67de2c4cf5da5f23865100ad159fba24
Misty Becker <mistyb01@uw.edu>
2023-01-11 20:13:34 -0800

    sidebar css progress

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,23 +4,35 @@ function Sidebar(props) {
     return (
         <section id="sidebar">
         <div className="step-section">
+          <span className={`step-circle${props.step === 1 ? ' active' : ''}`}>1</span>
+          <div className="step-text-wrapper">
           <span className="step-text">Step 1</span>
-          <span className="step-title">Your info</span>
+          <span className="step-desc">Your info</span>
+          </div>
         </div>
   
         <div className="step-section">
-          <span className="step-text">Step 2</span>
-          <span className="step-title">Select plan</span>
+          <span className={`step-circle${props.step === 2 ? ' active' : ''}`}>2</span>
+          <div className="step-text-wrapper">
+            <span className="step-text">Step 2</span>
+            <span className="step-title">Select plan</span>
+          </div>
         </div>
   
         <div className="step-section">
-          <span className="step-text">Step 3</span>
-          <span className="step-title">Add-ons</span>
+          <span className={`step-circle${props.step === 3 ? ' active' : ''}`}>3</span>
+          <div className="step-text-wrapper">
+            <span className="step-text">Step 3</span>
+            <span className="step-title">Add-ons</span>
+          </div>
         </div>
   
         <div className="step-section">
-          <span className="step-text">Step 4</span>
-          <span className="step-title">Summary</span>
+          <span className={`step-circle${props.step === 4 ? ' active' : ''}`}>4</span>
+          <div className="step-text-wrapper">
+            <span className="step-text">Step 4</span>
+            <span className="step-title">Summary</span>
+          </div>
         </div>
       </section>  
     )
