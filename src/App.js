8809e92c1adcc591ffd16492afaa2b7367f0c44a
Misty Becker <mistyb01@uw.edu>
2023-01-12 08:20:31 -0800

    main element styling

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
         handleNextStep={() => {setCurrentStep(prev => prev + 1)}} 
         handlePrevStep={() => {setCurrentStep(prev => prev - 1)}} />
 
-      <div className="attribution">
+      {/* <div className="attribution">
         Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
           >Frontend Mentor</a
         >. Coded by <a href="#">Your Name Here</a>.
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/steps/Step1.js
+++ src/components/steps/Step1.js
@@ -4,14 +4,22 @@ function Step1() {
     return (
         <section className="form-step-1">
             <h1>Personal info</h1>
-            <span>Please provide your name, email address, and phone number.</span>
-            <form>
-            <label htmlFor="fullName">Name</label>
-            <input type="text" name="fullName" placeholder="e.g. Stephen King"></input>
-            <label htmlFor="email">Email Address</label>
-            <input type="text" name="email" placeholder="e.g. stephenking@lorem.com"></input> 
-            <label htmlFor="phone">Phone Number</label> 
-            <input type="text" name="phone" placeholder="e.g. +1 234 567 890"></input>                                                                                                           
+            <p>Please provide your name, email address, and phone number.</p>
+            <form className="user-info-form">
+                <span>
+                    <label htmlFor="fullName">Name</label>
+                    <input type="text" id="fullName" name="fullName" placeholder="e.g. Stephen King"></input>
+                </span>
+
+                <span>
+                    <label htmlFor="email">Email Address</label>
+                    <input type="text" id="email" name="email" placeholder="e.g. stephenking@lorem.com"></input> 
+                </span>
+
+                <span>
+                    <label htmlFor="phone">Phone Number</label> 
+                    <input type="text" id="phone" name="phone" placeholder="e.g. +1 234 567 890"></input>    
+                </span>                                                                                                        
             </form>
       </section>
     )
