1849eb27e010a009844494d5540cf31682fd9f68
Misty Becker <mistyb01@uw.edu>
2023-01-13 13:28:02 -0800

    step 4 styling

--- src/components/steps/Step4.js
+++ src/components/steps/Step4.js
@@ -2,9 +2,37 @@ import React from "react";
 
 function Step4() {
     return (
-        <section className="form-container">
+        <section className="form-container results">
           <h1>Finishing up</h1> 
           <p>Double-check everything looks OK before confirming.</p>
+          <div className="results-table">
+
+            <div className="results-row">
+              <div>
+              <p><strong>Arcade (monthly)</strong></p>
+              <p className="change-text"><u>Change</u></p>
+              </div>
+              <p className="result-cost"><strong>$90/yr</strong></p>
+            </div>
+
+            <hr/>
+
+            <div className="results-row">
+              <p>Online service</p>
+              <p className="result-cost"><strong>+$10/yr</strong></p>
+            </div>
+
+            <div className="results-row">
+              <p>Larger storage</p>
+              <p className="result-cost"><strong>+$20/yr</strong></p>
+            </div>
+          </div>
+
+          <div className="results-row last">
+            <p>Total (per year)</p>
+            <p className="cost">$120/yr</p>
+          </div>
+
         </section>
     )
 }
