6c9a66a711ab9f208907b7407ba3814a1ddd37ba
Misty Becker <mistyb01@uw.edu>
2023-01-13 16:08:04 -0800

    css tinkering

--- src/components/steps/Step5.js
+++ src/components/steps/Step5.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { ReactComponent as CheckmarkIcon } from "../../assets/images/icon-checkmark.svg";
 
 function Step5() {
     return (
-        <section className="form-container">
-        Thank you! Thanks for confirming your subscription! We hope you have fun
+        <section className="form-container thank-you">
+            <h1>Thank you!</h1>
+            <p>
+                Thanks for confirming your subscription! We hope you have fun
         using our platform. If you ever need support, please feel free to email
         us at support@loremgaming.com.
+            </p>
         </section>
     )
 }
